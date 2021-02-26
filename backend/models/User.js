7c3258ac9d0e79df1d20f7593148e2544e8508e8
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-02-26 13:06:23 +0530

    branches creates

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  avatar: {
+    type: String,
+  },
+  verificationToken: {
+    type: String,
+  },
+  verificationValid: {
+    type: Date,
+  },
+});
+
+/*
+ * TODO: Add relevant middleware here to speed up and simplify processes
+ * Also look at relevant virtuals!
+ */
+
+export default model('user', UserSchema);
