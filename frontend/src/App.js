f6616a0ea869d1ff7bd627e4ee144d19f50d88dd
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-02-27 11:39:01 +0530

    little change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Login from "./components/login/Login.jsx";
+import Login from "./components/login/login.jsx";
 import "./App.css";
 import Home from "./components/Home/Home.jsx";
 
