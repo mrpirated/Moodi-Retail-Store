56eea9633709252e27bcca63ce92417e8711d4ed
shivani-cell <shivanimittal5152@gmail.com>
2021-03-05 02:11:42 +0530

    Billing page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 import Home from "./components/Home/Home.jsx";
 import Supplier from './components/Supplier/Supplier';
 import Customer from './components/Customer/Customer';
+import Billing from "./components/Billing/Billing.js";
 function App() {
   return (
     <div className="App">
-      <Customer/>
+      <Billing/>
     </div>
   );
 }
