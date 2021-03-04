9f146c26a2fa555d24220875331e2aea67c66d87
techimanshu8 <hs696984@gmail.com>
2021-03-04 14:06:47 +0530

    General Report Implemented

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import itemdetails from './routes/itemdetails';
 import addbatch from './routes/addbatches';
 import customerdetails from './routes/customerdetails';
 import supplierdetails from './routes/supplierdetails';
+import generalreport from './routes/generalreport';
 import config from 'config';
 import cors from 'cors';
 
@@ -31,6 +32,7 @@ app.use('/user/details',itemdetails);
 app.use('/user',addbatch);
 app.use('/user/customer',customerdetails);
 app.use('/user/supplier',supplierdetails);
+app.use('/user/report',generalreport);
 
 app.listen(PORT, () => {
   console.log('Go!');
