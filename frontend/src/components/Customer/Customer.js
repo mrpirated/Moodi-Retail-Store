717422923441c9e18ca6fbc51adcb26f080e6453
Swarnali Sinha <swarnali@mnnit.ac.in>
2021-03-11 20:47:47 +0530

    Customer page refactored

--- frontend/src/components/Customer/Customer.js
+++ frontend/src/components/Customer/Customer.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from '../Home/Navbar';
 import  './customer.css';
-import * as ReactBootstrap from 'react-bootstrap';
 import  {columns} from './tablecolumns.js';
 import Input from '../Input';
-import Table from './table'
 import DataTable from '../DataTable.js';
 function Customer() {
     const details=[{field:'Name'},{field:'Phone Number'},{field:'Customer ID:'},{field:'Address:'},{field:'Email ID:'},{field:'Ledger:'}];
     return (
-        <div className="customer">
+        <div>
             <Navbar title="Customer Details"/>
             {Search()}
             <hr className="hr-style"/>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                padding: '50px 40px 25px'
-                }}>
+            <div className="details" >
                  {details.map((item, index) => {
                             return (
                                <Input field={item.field} />
@@ -31,29 +24,12 @@ function Customer() {
     )
 }
  function Search(){
-    return(
-        <div classname="customer-form" >
-                <form className="form" style={{
-                            marginTop:'50px',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            fontSize:'25px'
-                            
-                    }}>
-                    <div className="field" >
-                        <label classname="name-label-customer" style={{
-                            paddingRight:'10px'
-                        }}>Customer Name: </label>
-                        <input type="text" style={{
-                            marginRight:'10px',
-                            fontSize:'25px'
-                        }} />
-                    </div>
-                    <button>Get Details</button>
-                </form>
-            </div>
-    )
+    return(<form className="form">
+                     <label  style={{paddingRight:'10px'}}>Customer Name: </label>
+                     <input type="text" style={{ marginRight:'10px', fontSize:'25px' }} />
+                     <button>Get Details</button>
+             </form>
+           )
 }
 
 export default Customer
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import './Customer/customer.css';
 export default function Input(props){
-    return ( <div>
-        <label classname="name-label-customer" style={{
-                    paddingRight:'1px'
-                }}>{props.field} </label>
-                <input type="text" style={{
-                    marginRight:'6px'
-                }} />
-    </div>);
+    return( <div style={{ padding:'30px 20px 30px 20px'}}>
+               <label>{props.field} </label>
+               <input type="text"/>
+            </div>);
 };
+     
