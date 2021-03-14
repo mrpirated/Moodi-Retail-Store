56763b0cddadca6281af9efe3b8a29b0d09646fc
Swarnali Sinha <swarnali@mnnit.ac.in>
2021-03-15 04:17:33 +0530

    further refactoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Customer from './components/Customer/Customer';
 ReactDOM.render(
   <React.StrictMode>
   <App/>
