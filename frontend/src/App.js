8ce1c740c2f58835479f5498a4ec31969e244c80
Swarnali Sinha <swarnali@mnnit.ac.in>
2021-03-15 04:09:32 +0530

    app refactored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,11 @@
-import React from "react";
-import Login from "./components/login/login.jsx";
 import "./App.css";
 import Home from "./components/Home/Home.jsx";
-import GeneralReport from "./components/GeneralReport/GeneralReport";
 import Supplier from "./components/Supplier/Supplier";
 import Customer from "./components/Customer/Customer";
-import Gstreport from "./components/Gstreport/Gstreport";
-import Expiryreport from "./components/Expiryreport/Expiryreport";
-import Damagedgood from "./components/Damagedgood/Damagedgood";
-import Purchasebill from "./components/Purchasebill/Purchasebill.js";
-//import SellBill from "./components/SellBill/SellBill";
-import Damagedgoodsreturnbill from "./components/Damagedgoodsreturnbill/Damagedgoodsreturnbill.js";
-import Customerreturn from "./components/Customerreturn/Customerreturn.js";
-import ExpiryReport from './components/Expiryreport/Expiryreport'
-import SellBill from './components/SellBill/SellBill';
 import Billing from './components/Billing';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import Report from './components/Reports';
-function App() {
+export default function App() {
 	return (
 		<Router>
 			<div className='App'>
@@ -35,4 +23,4 @@ function App() {
 	);
 }
 
-export default App;
+ 
