b7348edb02f3f946514e6b95f1c2bf483c9fb27f
Swarnali Sinha <swarnali@mnnit.ac.in>
2021-03-15 00:36:23 +0530

    reports in single page

--- frontend/src/components/Reports.js
+++ frontend/src/components/Reports.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import Navbar from './Home/Navbar';
 import GeneralReport from './GeneralReport/GeneralReport'
 import Gstreport from './Gstreport/Gstreport'
@@ -7,28 +8,23 @@ const currDate=new Date().toLocaleDateString();
 const currTime=new Date().toLocaleTimeString(); 
 
 export default function Report() {
-    let c=1;
-    
+    const [data,setdata]=useState({
+        display:<GeneralReport/>
+    });
+    const {display}=data;
                
     return (
         <div>
-         <Navbar title="Expiry Report"/>
-      <div style={{marginLeft:'10px',marginRight:'10px'}}>
-     <button> General Report</button> 
-     <button> GST Report</button> 
-     <button> Expiry Report</button> 
-     <button> Damaged Goods</button> 
-             <div style={{float:'right',marginTop:'10px',marginRight:'10px'}}>  Date : {currDate} ,Time:  {currTime}</div> 
-            <hr  className='hr-style'/>
-         </div>
-         {(() =>  { switch (c) {
-           case 1:<p>ahshs</p>;
-               
-               break;
-       
-           default:
-               break;
-       }})}
+            <Navbar title="Expiry Report"/>
+            <div style={{marginLeft:'10px',marginRight:'10px'}}>
+                <button onClick={()=>setdata({...data,display:<GeneralReport/>})}> General Report</button> 
+                <button onClick={()=>setdata({...data,display:<Gstreport/>})}> GST Report</button> 
+                <button onClick={()=>setdata({...data,display:<ExpiryReport/>})}> Expiry Report</button> 
+                <button onClick={()=>setdata({...data,display:<Damagedgood/>})}> Damaged Goods</button> 
+                <div style={{float:'right',marginTop:'10px',marginRight:'10px'}}>  Date : {currDate} ,Time:  {currTime}</div> 
+                <hr  className='hr-style'/>
+            </div>
+            {display}
          </div>
     )
 }
